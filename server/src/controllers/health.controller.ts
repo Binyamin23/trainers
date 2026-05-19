1a65c7e12d45e2dadb30ef69f5cffa218871b9b4
beny <benybar23@gmail.com>
2026-05-19 16:59:46 +0300

    remove console

--- server/src/controllers/health.controller.ts
+++ server/src/controllers/health.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 export const healthController = {
   check(_req: Request, res: Response) {
-    console.log('hi from server')
     res.json({ status: "ok" });
   },
 };
